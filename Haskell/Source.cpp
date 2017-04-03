96dfd6a7e3560fcbf091e965a3cc0920000ac6a8
Stephano <amunra@unm.edu>
2017-04-03 12:38:29 -0600

    Adds map

--- Haskell/Source.cpp
+++ Haskell/Source.cpp
@@ -2,17 +2,36 @@
 #include "Utility.h"
 #include <iostream>
 
+/**
 template<class Function, typename T>
 static List<T> filter(Function pred, const List<T> & list) {
 	if ( list.isNull() ) return List<T>();
 	else if ( pred( list.head() ) ) return List<T>{list.head()} + filter( pred, list.tail() );
 	else return filter( pred, list.tail() );
 }
+*/
+
+/**
+template<class Function1, class Function2, typename R = std::result_of<Function1(Function2)>::type>
+static auto compose(Function1 f, Function2 g) {
+	using I = std::result_of<Function2( R )>;
+	return [f, g]( I i ) -> R { return f( g( i ) ); };
+}
+*/
 
 int main()
 {
-	auto even = []( int i ) { return i % 2 == 0; };
-	std::cout << filter( even, List<int>( 1, 10 ) );
+	auto sqrtLambda = []( float num ) -> float { return sqrtf( num ); };
+	auto add1 = []( int num ) -> int { return num + 1; };
+	auto composed = [sqrtLambda, add1]( int num ) -> float { return sqrtLambda( add1( num ) ); };
+	std::cout << composed( 25 ) << std::endl;
+
+	auto even = []( int i ) -> bool { return i % 2 == 0; };
+	std::cout << filter( even, List<int>( 1, 10 ) ) << std::endl;
+
+	std::cout << map( []( int i ) { return List<int>{i}; }, List<int>( 1, 10 ) ) << std::endl;
+
+	std::cout << select( even, List<int>( 1, 4 ), List<char>( 'a', 'z' ) ) << std::endl;
 
 	while ( 1 )
 		;
--- Haskell/Utility.h
+++ Haskell/Utility.h
@@ -1,3 +1,32 @@
 #pragma once
 
 #include "List.h"
+
+template<class Lambda, typename T>
+List<T> filter( Lambda pred, const List<T> & list )
+{
+	if ( list.isNull() ) return List<T>();
+	else if ( pred( list.head() ) ) return List<T>{list.head()} + filter( pred, list.tail() );
+	else return filter( pred, list.tail() );
+}
+
+template<class Lambda, typename T, typename R = std::result_of<Lambda(T)>::type>
+List<R> map(Lambda proc, const List<T> & list) {
+	if ( list.isNull() ) return List<R>();
+	return List<R>{ proc( list.head() ) } + map( proc, list.tail() );
+}
+
+template<class Lambda, typename T, typename K>
+List<K> select(Lambda pred, const List<T> & predList, const List<K> & pollList) {
+	if ( predList.isNull() ) return List<K>();
+	else if ( pred( predList.head() ) ) {
+		return List<K>{pollList.head()} + select( pred, predList.tail(), pollList.tail() );
+	}
+	return select( pred, predList.tail(), pollList.tail() );
+}
+
+template<typename T, class Function1, class Function2, typename R = std::result_of<Function1( T )>::type>
+auto compose( Function1 f, Function2 g )
+{
+	return [f, g]( T i ) -> R { return f( g( i ) ); };
+}
