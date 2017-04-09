74a0c413f6af2eefc8547742d8c602ff3464d35e
Stephano <amunra@unm.edu>
2017-04-08 19:22:05 -0600

    Adds permutations function

--- Haskell/Homework5.cpp
+++ Haskell/Homework5.cpp
@@ -7,9 +7,32 @@ auto test = [](auto a)
     };
 };
 
+template<typename T>
+auto permutations(const List<T> & xs)
+{
+	if ( null( xs ) ) return hlist( xs );
+
+	return 
+	flatten(map(
+	[=]( auto x )
+	{
+		return 
+			map( [=]( auto y ) { return hlist( x ) + y; },
+				 permutations( filter( [=]( auto z )
+									   {
+									       return x != z;
+								       }, xs )));
+	}, xs
+	));
+}
+
 int main()
 {
     show(test(hlist(1,2,3))(hlist(4,5,6)));
+	show( permutations( hlist( "ABC" ) ) );
     show(filter([](auto x) { return x != ' '; }, hlist("hello what")));
+
+	while ( 1 )
+		;
 	return 0;
 }
