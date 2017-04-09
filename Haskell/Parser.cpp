27053e82303b6625169659eeb6710cc42cc587a6
Stephano <amunra@unm.edu>
2017-04-09 16:00:24 -0600

    Removes non-working code

--- Haskell/Parser.cpp
+++ Haskell/Parser.cpp
@@ -5,8 +5,6 @@ int main()
 	show( concat( map( replicate<int>( 4 ), hlsrange( 1, 4 ) ) ) );
 	show( concatMap( replicate<int>( 4 ), hlsrange( 1, 4 ) ) );
 
-	show( replicate( 5 )( hlist( "hello" ) ) );
-
 	while ( 1 )
 		;
 	return 0;
